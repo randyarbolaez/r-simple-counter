d52f033ff71c1977efb9fd4acb9592a336583c20
Randy <randyarbolaez0@gmail.com>
2019-05-12 11:55:52 -0400

    very simple react counter, nothing special

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increaseCount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  resetCount() {
+    this.setState({ count: 0 });
+  }
+
+  decreaseCount() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <div className="wrapper">
+          <button onClick={() => this.decreaseCount()}>Decrease</button>
+          <h1 className="count">{this.state.count}</h1>
+          <button onClick={() => this.increaseCount()}>Increase</button>
+        </div>
+        <div className="sub-wrapper">
+          <button onClick={() => this.resetCount()}>Reset</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
